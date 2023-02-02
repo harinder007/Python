414a347b3e523811073b0e85461ee5bb19938225
Harinder <96901742+harinder007@users.noreply.github.com>
2023-02-02 10:11:48 +0530

    Or Model

---
+++ maclopitOR.py
@@ -0,0 +1,11 @@
+#AND model in mucclloch model
+a=int(input("Enter first input"))
+b=int(input("Enter first input"))
+
+w1=10
+w2=10
+threshold=10
+if a*w1+b*w2>=threshold:
+    print(1)#True
+else:
+        print(0)#false
