93f3634c96a62f9252c393153e5e92eab260e3aa
Harinder <96901742+harinder007@users.noreply.github.com>
2023-02-02 10:11:28 +0530

    And model

---
+++ maclopitAND.py
@@ -0,0 +1,14 @@
+#AND model in mucclloch model
+a=int(input("Enter first input:"))
+b=int(input("Enter first input:"))
+
+w1=5
+w2=5
+threshold=10
+if a*w1+b*w2>=threshold:
+    print(1)
+else:
+        print(0)
+
+
+
