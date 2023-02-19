d8d2f24c7aab5e4eff04abed8d9f7a9070e68aa7
Harinder <96901742+harinder007@users.noreply.github.com>
2023-02-19 23:59:35 +0530

    Add files via upload

---
+++ arrayAcess.java
@@ -0,0 +1,8 @@
+class arrayAcess{
+    public static void main(String args[]){
+        int[] age={10,20,30,40};//creating an array
+        System.out.println("age is :"+age[2]);
+        System.out.println("age is :"+age[3]);
+
+    }
+}
