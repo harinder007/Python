9392022a1c9111e6291d5df7f0472d40c17980a0
Harinder <96901742+harinder007@users.noreply.github.com>
2023-02-25 23:53:26 +0530

    subtraction using argument

---
+++ sub.py
@@ -0,0 +1,6 @@
+def add_func(a,b):  
+    sub = a - b  
+    return sub 
+num1 = int(input("Enter the value of the first number: "))  
+num2 = int(input("Enter the value of the second number: "))  
+print("Subtract of two numbers: ",add_func(num1, num2))  
